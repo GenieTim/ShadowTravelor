239742731f00f25221f023f8614d4b74c7bfa869
FerhatSaritas <ferhat_saritas@yahoo.de>
2017-09-16 00:33:25 +0200

    Implemented Route and Routehistory, a list of routes. It can sort, add (append) and it has two getter methods, one for the list and one for single routes threw a given index number

--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/Route.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/Route.java
@@ -1,6 +1,10 @@
 package bernhardwebstudio.shadowtravelor.data;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Tim on 16.09.2017.
  */
@@ -8,10 +12,12 @@ public class Route {
 
     private ArrayList<LocationTimeConnection> route;
     private double score;
+    private GregorianCalendar date;
 
     public Route() {
         this.route = new ArrayList<LocationTimeConnection>();
         this.score = 0.0;
+        this.date = new GregorianCalendar();
     }
 
     public void add(LocationTimeConnection element){
@@ -24,5 +30,22 @@ public class Route {
 
     public double getScore(){
         return this.score;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public void setDate(GregorianCalendar date) {
+        this.date = date;
+    }
+
+    public void sort(){
+        Collections.sort(route, new Comparator<LocationTimeConnection>() {
+            @Override
+            public int compare(LocationTimeConnection locationTimeConnection, LocationTimeConnection t1) {
+                return locationTimeConnection.getDatetime().compareTo(t1.getDatetime());
+            }
+        });
     }
 }
--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/RouteHistory.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/RouteHistory.java
@@ -1,8 +1,37 @@
 package bernhardwebstudio.shadowtravelor.data;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Created by Tim on 16.09.2017.
  */
 public class RouteHistory {
+    private ArrayList<Route> routes;
 
+    public RouteHistory(){
+        this.routes = new ArrayList<Route>();
+    }
+
+    public void add(Route route){
+        this.routes.add(route);
+    }
+
+    public void sort(){
+        Collections.sort(routes, new Comparator<Route>() {
+            @Override
+            public int compare(Route route, Route t1) {
+                return route.getDate().compareTo(t1.getDate());
+            }
+        });
+    }
+
+    public ArrayList<Route> getRoutes() {
+        return routes;
+    }
+
+    public Route getRoute(int i) {
+        return this.routes.get(i);
+    }
 }
