d077c4f14a19b95cbcbe523694765c2de567eb91
Tim Bernhard <tim@bernhard-webstudio.ch>
2017-09-16 01:49:58 +0200

    add GraphView to dependency

--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/Diagram.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/Diagram.java
@@ -4,4 +4,10 @@ package bernhardwebstudio.shadowtravelor.data;
  * Created by Tim on 16.09.2017.
  */
 public class Diagram {
+    private Route route;
+
+    public Diagram(Route route) {
+
+    }
+
 }
--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/RouteHistory.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/RouteHistory.java
@@ -4,4 +4,5 @@ package bernhardwebstudio.shadowtravelor.data;
  * Created by Tim on 16.09.2017.
  */
 public class RouteHistory {
+
 }
