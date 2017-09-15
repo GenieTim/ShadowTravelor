5a920b920c0b1f96a5e737e2b629c87bd53ef14d
Tim Bernhard <tim@bernhard-webstudio.ch>
2017-09-16 00:49:37 +0200

    implement Location Class

--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/Location.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/Location.java
@@ -4,4 +4,22 @@ package bernhardwebstudio.shadowtravelor.data;
  * Created by Tim on 16.09.2017.
  */
 public class Location {
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
