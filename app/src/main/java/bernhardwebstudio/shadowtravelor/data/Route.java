ea0725ebd1faddd61342c613a8535bbe9e74cbec
FerhatSaritas <ferhat_saritas@yahoo.de>
2017-09-15 23:39:18 +0200

    inplemented Route with the necessaty getter and setter methods

--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/Route.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/Route.java
@@ -1,7 +1,28 @@
 package bernhardwebstudio.shadowtravelor.data;
 
+import java.util.ArrayList;
 /**
  * Created by Tim on 16.09.2017.
  */
 public class Route {
+
+    private ArrayList<LocationTimeConnection> route;
+    private double score;
+
+    public Route() {
+        this.route = new ArrayList<LocationTimeConnection>();
+        this.score = 0.0;
+    }
+
+    public void add(LocationTimeConnection element){
+        this.route.add(element);
+    }
+
+    public void setScore(double score){
+        this.score = score;
+    }
+
+    public double getScore(){
+        return this.score;
+    }
 }
