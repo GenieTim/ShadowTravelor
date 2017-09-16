c6866fc997a1c1c0e35f706c91cea20588070b0e
bene13 <lindnerbenedict@aol.de>
2017-09-16 02:25:16 +0200

    DB

--- app/src/main/java/bernhardwebstudio/shadowtravelor/database/DBHelper.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/database/DBHelper.java
@@ -9,6 +9,35 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBHelper extends SQLiteOpenHelper {
 
+    public static final String TABLE_LOCATION = "Location";
+    public static final String COLUMN_LONG = "Longitude";
+    public static final String COLUMN_LAT = "Latitude";
+
+    public static final String TABLE_LOC_TIME_CON = "LocationTime";
+    public static final String COLUMN_TIME = "Time";
+    public static final String COLUMN_LOC = "Location";
+
+    public static final String TABLE_ROUTE = "Route";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_SCORE = "Score";
+
+    public static final String TABLE_ROUTE_POINTS = "RoutePoints";
+    public static final String COLUMN_ID_ROUTE = "ID_Route";
+    public static final String COLUMN_ID_POINT = "ID_Point";
+
+    public static String createTableLocation =
+            "CREATE TABLE " + TABLE_LOCATION + "(" + COLUMN_ID + "Integer Primary Key ," + COLUMN_LAT +
+                    "real," + COLUMN_LONG + "real);";
+    public static String createTableLocationTime =
+            "CREATE TABLE " + TABLE_LOC_TIME_CON + "(" + COLUMN_ID + "Integer Primary Key ," + COLUMN_LOC +
+                    "real," + COLUMN_LONG + "real);";
+    public static String createTableRoute =
+            "CREATE TABLE " + TABLE_LOCATION + "(" + COLUMN_ID + "Integer Primary Key ," + COLUMN_LAT +
+                    "real," + COLUMN_LONG + "real);";
+    public static String createTableRoutePoints =
+            "CREATE TABLE " + TABLE_LOCATION + "(" + COLUMN_ID + "Integer Primary Key ," + COLUMN_LAT +
+                    "real," + COLUMN_LONG + "real);";
+
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
--- app/src/main/java/bernhardwebstudio/shadowtravelor/service/BootService.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/service/BootService.java
@@ -1,18 +1,26 @@
 package bernhardwebstudio.shadowtravelor.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.SystemClock;
 
 public class BootService extends BroadcastReceiver {
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent startServiceIntent = new Intent(context, PositionService.class);
-        context.startService(startServiceIntent);
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent alarmIntent = new Intent(context, NetworkChangedService.class);
+        PendingIntent pending = PendingIntent.getBroadcast(context, 0, intent, 0);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), 6000, pending);
+
+        /*Intent startServiceIntent = new Intent(context, PositionService.class);
+        context.startService(startServiceIntent);*/
     }
 
 }
