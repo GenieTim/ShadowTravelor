5493a23a4ec75e0f32514413b1fc0e60f43c34ba
Tim Bernhard <tim@bernhard-webstudio.ch>
2017-09-16 09:28:21 +0200

    implement Profile clas

--- app/src/main/java/bernhardwebstudio/shadowtravelor/data/Profile.java
+++ app/src/main/java/bernhardwebstudio/shadowtravelor/data/Profile.java
@@ -1,10 +1,20 @@
 package bernhardwebstudio.shadowtravelor.data;
 
+import java.util.ArrayList;
+
 /**
  * Created by Tim on 16.09.2017.
  */
 
 public class Profile {
 
+    private ArrayList<ProfileDay> calendar;
 
+    public ArrayList<ProfileDay> getCalendar() {
+        return calendar;
+    }
+
+    public void addDay(ProfileDay day) {
+        this.calendar.add(day);
+    }
 }
